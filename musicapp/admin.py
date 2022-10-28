7ba720e6b1263e83cd93ba8491185e5de3cd71c5
unknown <emmizychuks@gmail.com>
2022-10-28 07:54:49 +0100

    made changes in the models.py, admin.py, and settings.py.

--- musicapp/admin.py
+++ musicapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Song, Lyric, Artiste
 
 # Register your models here.
+
+admin.site.register (Song)
+admin.site.register (Lyric)
+admin.site.register (Artiste)
